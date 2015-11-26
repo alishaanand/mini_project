0ef416ec848f3edfe56bf41c5fa445d7149e35fc
alishaanand <anandalisha2958@gmail.com>
2015-11-26 19:11:41 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,8 @@
+
+<!DOCTYPE html>
+<html lang='en'>
+	<body background="book.jpg">
+	<h2><font color="black">Please enter year between 1944 and 2013</font></h2>
+	<h2><font color="black">Please enter gender as male,female or both in case of top search</font></h2>
+	<h2><font color="black">Please enter gender as male or female in case of popularity</font></h2>
+	</body>
