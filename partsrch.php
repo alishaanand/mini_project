c21c498b371d968e723074f7010bf7586fd674aa
alishaanand <anandalisha2958@gmail.com>
2015-11-26 19:28:22 +0530

    Update partsrch.php

--- partsrch.php
+++ partsrch.php
@@ -1,5 +1,5 @@
 <?php 
-include"connection.php";
+include"connect.php";
 if(isset($_POST['search']))
 {
 $search=$_POST['search'];
@@ -7,7 +7,7 @@ $sql="SELECT * from babyname WHERE given_name = ' " .@mysql_real_escape_string( 
 $result=@mysql_query($sql) or die(@mysql_error());
 $number =@mysql_num_rows($result);
 echo ".$number.";
-include"baby.php";
+include"home.php";
 print <<< HERE
 <h2>RESULT</h2>
 <h3>$search is present $number times</h3> 
