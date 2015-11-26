f2d1350ec50a4406136094470bd471dcc578a9ac
alishaanand <anandalisha2958@gmail.com>
2015-11-26 19:23:27 +0530

    Update and rename connection.php to cnt.php

--- cnt.php
+++ cnt.php
@@ -5,12 +5,10 @@ $db_host = 'localhost';
 $db_user = 'root';
 $db_pwd = ''; 
 
-
 $database = 'project';
-$table ='babydata';
+$table ='names';
 $conn = @mysql_connect($db_host, $db_user, $db_pwd) or
 die("cant select database ");
-
 
 if (!mysql_select_db($database))
 die("cant select database");
